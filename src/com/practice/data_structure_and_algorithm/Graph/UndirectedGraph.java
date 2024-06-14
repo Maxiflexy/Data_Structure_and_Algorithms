6c2c68d6218759c2b7cc92c83aa801787abd3623
Maxiflexy <onyemax247@gmail.com>
2024-06-14 16:08:53 +0100

    graph commit

---
+++ src/com/practice/data_structure_and_algorithm/Graph/UndirectedGraph.java
@@ -0,0 +1,46 @@
+package com.practice.data_structure_and_algorithm.Graph;
+
+public class UndirectedGraph {
+
+    private int V;  //number of vertices in the graph
+    private int E;  //number of edges in the graph
+    private int[][] adjMatrix;
+
+    public UndirectedGraph(int nodes){
+        this.V = nodes;
+        this.E = 0;
+        adjMatrix = new int[nodes][nodes];
+    }
+
+    public void addEdge(int u, int v){
+        adjMatrix[u][v] = 1;
+        adjMatrix[v][u] = 1;
+        E++;
+    }
+
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(V + "vertices, " + E + " edges " + "\n");
+        for(int v = 0; v < V; v++){
+            stringBuilder.append(v + ": ");
+            for(int w : adjMatrix[v]){
+                stringBuilder.append(w + " ");
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+
+        UndirectedGraph graph = new UndirectedGraph(4);
+        graph.addEdge(0, 1);
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 0);
+        System.out.println(graph);
+
+
+    }
+}
